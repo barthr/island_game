bc4b22870223ddb5c91e7a5288005e26c0a62bf2
bartfokker <bart@publicsonar.com>
2018-06-02 12:39:20 +0200

    added board + island.ex

--- lib/island_engine/board.ex
+++ lib/island_engine/board.ex
@@ -1,3 +1,12 @@
 defmodule IslandsEngine.Board do
+  alias IslandsEngine.Island
+
   def new(), do: %{}
+
+  def position_island(board, key, %Island{} = island) do
+    case overlaps_existing_island?(board, key, island) do
+      true -> {:error, :overlapping_island}
+      false -> Map.put(board, key, island)
+    end
+  end
 end
